8e1cfc5845861a7b763a6b7776e2e8391b606561
SHAILY PRIYADARSHINI <54751648+Shaily-Priyadarshini@users.noreply.github.com>
2022-05-17 07:15:12 +0530

    Added solution - LeetHub

---
+++ Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.py
@@ -0,0 +1,39 @@
+#User function Template for python3
+
+class Solution:
+    def sort012(self,a,n):
+        # code here
+        l=0
+        h=n-1
+        m=0
+        
+        while m<=h:
+            if a[m]==0:
+                a[l],a[m]=a[m],a[l]
+                l=l+1
+                m=m+1
+                
+            elif a[m]==1:
+                m=m+1
+            else:
+                a[m],a[h]=a[h],a[m]
+                h=h-1
+        return a
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t=int(input())
+    for _ in range(t):
+        n=int(input())
+        arr=[int(x) for x in input().strip().split()]
+        ob=Solution()
+        ob.sort012(arr,n)
+        for i in arr:
+            print(i, end=' ')
+        print()
+
+# } Driver Code Ends
